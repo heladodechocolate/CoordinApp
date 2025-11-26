8339bcb0cf3aa6c8b46ade19feefff151355d3dc
heladodechocolate <leonardorestrepo201@gmail.com>
2025-11-25 21:32:56 -0300

    corrección de un nombre

--- Backend/src/routes/reporteRoutes.js
+++ Backend/src/routes/reporteRoutes.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/authMiddleware');
-const { getHistorialCambiosCompleto } = require('../controllers/reporteController');
+const { getHistorialCambiosCompleto } = require('../controllers/reporteController.js');
 
 // Ruta de prueba para obtener todos los datos de historial_cambios (protegida por autenticación)
 router.get('/reportes/prueba-historial', authMiddleware, getHistorialCambiosCompleto);
